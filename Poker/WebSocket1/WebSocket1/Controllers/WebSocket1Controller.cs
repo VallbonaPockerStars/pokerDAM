e10cd1959c398f6b73312b34ac1a71f11b1e9bb3
eriicfm <eric_fontmateu@iescarlesvallbona.cat>
2024-01-25 20:58:30 +0100

    Cambio de metodos publicos estaticos

--- Poker/WebSocket1/WebSocket1/Controllers/WebSocket1Controller.cs
+++ Poker/WebSocket1/WebSocket1/Controllers/WebSocket1Controller.cs
@@ -6,11 +6,17 @@ using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http;
+using WebSocket1.Models;
 
 namespace WebSocket1.Controllers
 {
+
+
+
     public class WebSocketController : ApiController
     {
+       public static Baraja baraja = new Baraja();
+
         public HttpResponseMessage Get(string nom)
         {
             // La crida al websocket serà del tipus   ws://host:port/api/websocket?nom=Pere
@@ -20,6 +26,7 @@ namespace WebSocket1.Controllers
 
         private class SocketHandler : WebSocketHandler
         {
+
             private static readonly WebSocketCollection Sockets = new WebSocketCollection();
             private static List<WebSocketHandler> Sockets2 = new List<WebSocketHandler>();
 
@@ -30,11 +37,31 @@ namespace WebSocket1.Controllers
                 _nom = nom;
             }
 
+            // metodo para repartir cartas
+            public static void repartirCartas()
+            {
+                // crea un array de clase cartas y guarda las 52 cartas
+                
+            }
+
             public override void OnOpen()
             {
                 // Cuando un nuevo usuario se conecta: agregar el SocketHandler a la colección, notificar a todos la incorporación y darle la bienvenida
                 Sockets2.Add(this);
                 Send(": " + "Conectado");
+
+                Random random = new Random();
+                List<string> mano = new List<string>();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    int index = random.Next(0, baraja.LongitudBaraja());
+                    Carta carta = baraja.QuitarCarta(index);
+                    mano.Add(carta.carta);
+                }
+
+                string mensaje = "Tu mano de poker: " + string.Join(" ", mano);
+                Send(mensaje);
             }
 
             public override void OnMessage(string mensaje)
@@ -42,7 +69,7 @@ namespace WebSocket1.Controllers
                 // Cuando un usuario envía un mensaje, asegurarse de que todos lo reciban
                 foreach (var socket in Sockets2)
                 {
-                    socket.Send(_nom+ ": " + mensaje);
+                    socket.Send(_nom + ": " + mensaje);
                 }
             }
 
---
+++ Poker/WebSocket1/WebSocket1/models/Baraja.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace WebSocket1.Models
+{
+    using System;
+
+    public class Baraja
+    {
+       
+
+        private Carta[] _baraja;
+        private string[] _cartas = new string[]
+        {
+        "\U0001F0A1", "\U0001F0A2", "\U0001F0A3", "\U0001F0A4", "\U0001F0A5", "\U0001F0A6", "\U0001F0A7", "\U0001F0A8", "\U0001F0A9", "\U0001F0AA", "\U0001F0AB", "\U0001F0AD", "\U0001F0AE", // Corazones (Hearts)
+        "\U0001F0B1", "\U0001F0B2", "\U0001F0B3", "\U0001F0B4", "\U0001F0B5", "\U0001F0B6", "\U0001F0B7", "\U0001F0B8", "\U0001F0B9", "\U0001F0BA", "\U0001F0BB", "\U0001F0BD", "\U0001F0BE", // Diamantes (Diamonds)
+        "\U0001F0C1", "\U0001F0C2", "\U0001F0C3", "\U0001F0C4", "\U0001F0C5", "\U0001F0C6", "\U0001F0C7", "\U0001F0C8", "\U0001F0C9", "\U0001F0CA", "\U0001F0CB", "\U0001F0CD", "\U0001F0CE", // Tréboles (Clubs)
+        "\U0001F0D1", "\U0001F0D2", "\U0001F0D3", "\U0001F0D4", "\U0001F0D5", "\U0001F0D6", "\U0001F0D7", "\U0001F0D8", "\U0001F0D9", "\U0001F0DA", "\U0001F0DB", "\U0001F0DD", "\U0001F0DE"  // Picas (Spades)
+        };
+
+        private int[] _valores = new int[]
+        {
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, // Corazones (Hearts)
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, // Diamantes (Diamonds)
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, // Tréboles (Clubs)
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13  // Picas (Spades)
+        };
+
+        private string[] _palo = new string[] { "corazones", "diamantes", "treboles", "picas" };
+
+        public Baraja()
+        {
+            _baraja = new Carta[52];
+            InicializarBaraja();
+        }
+
+        // Método para inicializar la baraja
+        private void InicializarBaraja()
+        {
+            for (int i = 0; i < _baraja.Length; i++)
+            {
+                _baraja[i] = new Carta(i.ToString(), _palo[i / 13], _valores[i], _cartas[i]);
+            }
+        }
+
+        // Método para quitar una carta de la baraja
+        public Carta QuitarCarta(int index)
+        {
+            if (index >= 0 && index < _baraja.Length && _baraja[index] != null)
+            {
+                Carta carta = _baraja[index];
+                _baraja[index] = null;
+                return carta;
+            }
+            else
+            {
+                Console.WriteLine("Índice inválido o carta ya retirada.");
+                return null;
+            }
+        }
+
+        // Metodo que devuelve la longitud de la baraja actualizada por si se quita una carta
+        public int LongitudBaraja()
+        {
+            int longitud = 0;
+            for (int i = 0; i < _baraja.Length; i++)
+            {
+                if (_baraja[i] != null)
+                {
+                    longitud++;
+                }
+            }
+            return longitud;
+        }
+
+        
+    }
+
+}
